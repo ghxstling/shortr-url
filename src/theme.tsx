7c63d33ab423addb43f8410f1aff420bcbe68c7f
ghxstling <dcho282@aucklanduni.ac.nz>
2024-11-12 18:24:47 +1300

    refactor: moved common CSS themes to theme.tsx

---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const centerContent = {
+  display: "flex",
+  textAlign: "center",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "Ubuntu",
+  },
+});
