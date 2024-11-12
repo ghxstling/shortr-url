508476648128cd8649db69ae61fc9d68b76dcd4c
ghxstling <dcho282@aucklanduni.ac.nz>
2024-11-12 13:29:46 +1300

    feat(wip): implement basic layout of site

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import {
   Button,
@@ -9,52 +8,84 @@ import {
   Grid2,
   Box,
   Stack,
+  Paper,
 } from "@mui/material";
+
+const centerContent = {
+  display: "flex",
+  textAlign: "center",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 function App() {
   return (
     <>
       <Stack spacing={5}>
-        <Box>
-          <Typography variant="h1">Shortr</Typography>
+        <Box padding={10}>
+          <Typography
+            variant="h1"
+            sx={{
+              fontFamily: ["sans-serif", "Ubuntu"],
+              fontWeight: "bold",
+            }}
+          >
+            ShortrURL
+          </Typography>
           <Typography variant="overline">Made by ghxstling</Typography>
         </Box>
         <Box>
-          <Grid2
-            container
-            direction={"row"}
-            spacing={5}
-            sx={{ justifyContent: "center" }}
+          <Paper
+            elevation={10}
+            sx={{
+              height: "10em",
+              bgcolor: "white",
+              color: "black",
+              borderRadius: 3,
+              ...centerContent,
+            }}
           >
-            <TextField
-              variant="outlined"
-              placeholder="www.example.com"
-              sx={{
-                width: "50vh",
-              }}
-            />
-            <Button
-              variant="contained"
-              sx={{
-                bgcolor: "white",
-                color: "black",
-              }}
-            >
-              Shorten URL!
-            </Button>
-          </Grid2>
+            <Grid2 container spacing={1}>
+              <Grid2 size={20}>
+                <Typography variant="body1">Paste your URL Here:</Typography>
+              </Grid2>
+              <Grid2 size={10}>
+                <TextField
+                  variant="outlined"
+                  placeholder="www.example.com"
+                  sx={{
+                    width: "30em",
+                  }}
+                />
+              </Grid2>
+              <Grid2 size={2}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    bgcolor: "#3d6ecf",
+                    color: "white",
+                  }}
+                >
+                  Shorten URL!
+                </Button>
+              </Grid2>
+            </Grid2>
+          </Paper>
         </Box>
-        <Box
-          sx={{
-            width: "85vh",
-            height: "40vh",
-            bgcolor: "white",
-            color: "black",
-            padding: 1,
-            borderRadius: 5,
-          }}
-        >
-          dsaddasdsad
+        <Box>
+          <Paper
+            elevation={10}
+            sx={{
+              width: "60em",
+              height: "30em",
+              bgcolor: "white",
+              color: "black",
+              padding: 1,
+              borderRadius: 3,
+            }}
+          >
+            INFORMATION
+          </Paper>
         </Box>
       </Stack>
     </>
